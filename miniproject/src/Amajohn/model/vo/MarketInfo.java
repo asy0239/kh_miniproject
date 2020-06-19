eedc6442a01f714ffeec9b00c2edecee547b0f09
BG_CHO <BG_CHO@BG-CHO-PC>
2020-06-19 15:43:48 +0900

    06.19. 기본 클래스 생성본
    클래스 다이어그램 참조.

---
+++ miniproject/src/Amajohn/model/vo/MarketInfo.java
@@ -0,0 +1,10 @@
+package Amajohn.model.vo;
+
+public class MarketInfo {
+	// Maps 클래스 이용 예정
+	
+	private long palyTime;
+	
+	// 생성자
+	// getter & setter
+}
---
+++ miniproject/src/Amajohn/model/vo/Product.java
@@ -0,0 +1,14 @@
+package Amajohn.model.vo;
+
+public class Product {
+	private String productName;
+	private int productPrice;
+	private int productCtn;
+	private long productWholeLifetime;
+	private long productRestLifetime;
+
+
+	// 생성자
+	// getter & setter
+
+}
